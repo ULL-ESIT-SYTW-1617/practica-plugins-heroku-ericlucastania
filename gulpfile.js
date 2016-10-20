dbf8e774d48f7544f4729d809c9d9bdf25943970
alu0100785265 <alu0100785265@ull.edu.es>
2016-10-20 23:14:12 +0000

    salida

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,12 @@ var gulp = require('gulp');
 var ejs = require("gulp-ejs");
 var dep = require('dep')
  
- gulp.task('ejs', function() {
-
-    gulp.src("./template/package.ejs")
-        .pipe(ejs({
-            autor: {
-                name: "Eric"
-            }
-        }))
-        .pipe(gulp.dest("./dist"));
- });
  
- gulp.task('ejs', function() {
-
-    gulp.src("./template/package.ejs")
-        .pipe(ejs({
-            autor: {
-                name: "Eric"
-            }
-        }))
-        .pipe(gulp.dest("./dist"));
- });
+ 
  
  
  gulp.task('deploy-iaas', function(){
+     
      
      
  })
