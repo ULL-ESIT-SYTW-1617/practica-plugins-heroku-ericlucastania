82a0760a2d17fd9c9e5287e92f82f5f9bbc5c38f
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-27 18:35:55 +0000

    undefined

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -32,7 +32,7 @@ gitConfig(function (err, config) { //PARA RECOGER OPCIONES POR DEFECTO
 	
 	
 	if (comprobar.comp(argv)){
-		iniDeplo.initializes(path,direct,fs,'deploy');
+		iniDeplo.execute(path,direct,fs,'deploy');
 		renderTemplate.rend(argv,path,fs,defaultname,defaultemail,direct);
 	}
 	else {
