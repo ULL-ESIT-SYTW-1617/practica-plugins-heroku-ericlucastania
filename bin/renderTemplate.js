64dc962af88f54343ca23707017e1a71dd5cc069
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-27 17:25:42 +0000

    no relevants changes

---
+++ bin/renderTemplate.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = {
+
+	rend: (argv,path,fs,defaultname,defaultemail,direct) => {
+		
+		
+		var ejs = require('ejs');
+		
+		
+		//Rutas interesantes
+		var rutaTemplate = path.join(__dirname, '..','template');
+		var pck = require(path.join(__dirname, '..','package.json'));
+		//Expresion regular
+		
+		var re = /.ejs/g;
+	
+		
+		// Si la opcion es -v,imprime version
+		
+		if(argv.v){
+			console.log(pck.version);
+		}
+		
+		// Creamos la carpeta
+		
+		var dir = argv.dir || defaultname;
+		fs.mkdirsSync(direct + dir);
+		
+		//Ver los nombres de los archivos dentro de las carpetas
+		var names = fs.readdirSync(rutaTemplate);
+		
+		
+		
+		var recursive = (names,folder) => {
+			for (var i in names){
+			
+				if(names[i].match(re)){
+				
+					//Renderizamos el fichero
+					var data = ejs.renderFile(rutaTemplate + '/' + folder + names[i],{
+						
+						autor:{
+							name: argv.a || defaultname,
+							email: argv.e || defaultemail
+						}
+						
+					},(err,data) => {
+						if(err){
+							throw err;
+							
+						} else{
+							return data;
+							
+						}
+					});
+					
+					//Sustituimos el nombre, para quitarle la extensión ejs
+					
+					var newstr = names[i].replace(re, '');
+				   
+					fs.writeFile(direct + dir + '/' + folder + newstr, data, (err) => {
+					  if (err) throw err;
+					});
+				}
+				else{
+					fs.mkdirsSync(direct + dir + '/' +names[i]);
+					recursive(fs.readdirSync(rutaTemplate + '/' + names[i]),names[i] + '/');
+				}
+			}
+		};
+		
+		recursive(names,'');
+	}
+};
---
+++ gh-pages/template/gulpfile.js.ejs
@@ -0,0 +1,24 @@
+//GULP
+
+var gulp  = require('gulp');
+var shell = require('gulp-shell');
+ 
+ 
+
+gulp.task('gitbook', function()
+{
+    return gulp.src('./scripts')
+        .pipe(shell([
+            "gitbook install"
+        ]))        
+        .pipe(shell([
+           "./scripts/losh generate-gitbook && generate-wiki && deploy-gitbook && deploy-wiki" 
+        ]))
+           
+       
+});
+
+
+gulp.task('default', function(){
+    gulp.watch(['txt/*.md', 'scripts/**',  'book.json'], ['gitbook']); 
+});
